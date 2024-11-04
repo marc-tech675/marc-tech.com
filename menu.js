d08e3a0b072a675b38395d418780e95c97ae7bf5
marc-tech675 <286plug@gmail.com>
2024-11-04 15:36:45 -0800

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,15 @@
+const menu = `
+  *Menu du bot*
+
+  **Gestion des utilisateurs**
+  /connect : Se connecter
+  /deconnect : Se déconnecter
+
+  **Gestion des messages**
+  /envoyer : Envoyer un message
+  /effacer : Effacer un message
+
+  **Gestion des groupes**
+  /kickall : Supprimer tous les membres
+  /promouvoir : Promouvoir un membre
+`;
