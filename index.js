7eeac31b3e7f3224a9ccbfca10fc7d9940d1fd85
marc-tech675 <286plug@gmail.com>
2024-11-04 15:16:10 -0800

    Update index.js

--- index.js
+++ index.js
@@ -47,3 +47,38 @@ client.on('ready', () => {
   console.log('Bot prêt !');
 });
 ```
+
+
+const { Client, MessageMedia } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('Bot connecté !');
+});
+
+client.on('message', (message) => {
+  if (message.body === '/connect') {
+    const code = client.generateQRCode();
+    console.log(`Code de connexion : ${code}`);
+  } else if (message.body === '/envoyer') {
+    const numero = 'NUMERO_DESTINATAIRE';
+    const messageText = 'Bonjour !';
+    client.sendMessage(numero, messageText);
+  } else if (message.body === '/kickall') {
+    // Supprimer tous les membres du groupe
+    const groupeId = 'ID_DU_GROUPE';
+    client.getGroup(groupeId).then((groupe) => {
+      groupe.members.forEach((membre) => {
+        client.removeParticipant(groupeId, (link unavailable));
+      });
+    });
+  }
+});
+
+client.initialize();
